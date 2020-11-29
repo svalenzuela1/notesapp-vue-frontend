977ae88a1402714f2732f343490fa15127567b54
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-11-28 23:57:51 -0500

    have not fetched backend yet

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,17 +3,17 @@
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
-            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
+            src="https://cdn1.iconfinder.com/data/icons/ios-11-ui-elements-vol-2/29/notes_ios_x_iphone_ui-103-512.png"
+            alt="notes"
         >
       </b-navbar-item>
     </template>
     <template slot="start">
       <b-navbar-item href="#">
-        Home
+        <router-link to="/">Home</router-link>
       </b-navbar-item>
       <b-navbar-item href="#">
-        Documentation
+        <router-link to="/about">About</router-link>
       </b-navbar-item>
       <b-navbar-dropdown label="Info">
         <b-navbar-item href="#">
@@ -28,9 +28,9 @@
     <template slot="end">
       <b-navbar-item tag="div">
         <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
+<!--          <a class="button is-primary">-->
+<!--            <strong>Sign up</strong>-->
+<!--          </a>-->
           <a class="button is-light">
             Log in
           </a>
@@ -47,5 +47,14 @@ name: "Header"
 </script>
 
 <style scoped>
+nav {
+  width: 80%;
+  margin: 10px auto;
+}
+
+img{
+  height: 50px;
+  width: 30px;
+}
 
 </style>
