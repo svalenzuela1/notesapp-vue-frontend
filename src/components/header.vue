aa55dcaa614a264065ee5e3d1b9fed356453e13e
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-11-29 00:13:52 -0500

    route to login currently being set up

--- src/components/header.vue
+++ src/components/header.vue
@@ -31,9 +31,10 @@
 <!--          <a class="button is-primary">-->
 <!--            <strong>Sign up</strong>-->
 <!--          </a>-->
-          <a class="button is-light">
-            Log in
-          </a>
+<!--          <a class="button is-light">-->
+<!--            Log in-->
+<!--          </a>-->
+          <router-link to="/login">Log in</router-link>
         </div>
       </b-navbar-item>
     </template>
