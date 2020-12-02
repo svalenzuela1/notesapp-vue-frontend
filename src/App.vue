242993b11f53d4df561471487ff843e13235afb5
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-12-01 23:59:47 -0500

    configuring

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
       this.loggedIn = true
       this.token = event
       //suppose to switch pages
-      this.$router.push('/')
+      this.$router.push('/about')
     },
     logout: function(){
       this.loggedIn = false
