68027cfb97604a8fc7b479e3a3ea8118d8ab3405
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-12-08 02:48:03 -0500

    data not logging to console

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="about">
     <h1>Hello</h1>
+    <button v-on:click="getNote">Test</button>
   </div>
 </template>
+
 
 <script>
 export default {
   name: "Notes",
+  props: true,
+  token: null,
+  URL: 'http://localhost:3000',
+  data: function(){
+  return {
+    notes: [],
+    message: ''
+  }
+}
+,
+  methods: {
+    getNote: function(){
 
+      fetch(`${this.URL}/notes`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.token}`
+        }
+        .then(response => response.JSON())
+        .then(data => {
+          console.log("working", data)
+        })
+      })
+    }
+  }
 }
 </script>
