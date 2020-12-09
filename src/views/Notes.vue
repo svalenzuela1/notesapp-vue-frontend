38a804db3d9c027c8bbf97fb267e910f9a4ac226
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-12-09 02:58:47 -0500

    delete note working

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -4,26 +4,30 @@
 <!--      <li v-for="note of notes" v-bind:key="note.id">{{note.message}}</li>-->
       <b-message class="postedNotes" v-for="note of notes" v-bind:key="note.id" title="Notes" type="is-dark" has-icon icon="account" aria-close-label="Close message">
         {{note.message}}
+
+        <p class="control">
+          <button class="button is-danger" v-bind:id="note.id" v-on:click="deleteNote">
+            Delete Post
+          </button>
+        </p>
       </b-message>
+
+
     </ul>
 
-    <div class="field is-grouped" id="saveAndDelete">
+    <div class="newNote">
+    <b-input placeholder="Enter New Message"
+             size="is-medium"
+             icon="account"
+            v-model="message">
+    </b-input>
       <p class="control">
-        <button class="button is-link">
+        <button class="button is-link" v-on:click="newNote">
           Save Post
         </button>
       </p>
-      <p class="control">
-        <button class="button">
-          Cancel
-        </button>
-      </p>
-      <p class="control">
-        <button class="button is-danger">
-          Delete Post
-        </button>
-      </p>
     </div>
+
   </div>
 
 </template>
@@ -39,21 +43,50 @@ export default {
   }
 },
   created: function(){
-
-    fetch('http://localhost:3000/notes', {
-      method: 'get',
-      headers: {
-        Authorization: `bearer ${this.$route.query.token.token}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.notes = data
-    })
+      this.getNotes()
   },
   methods: {
+    getNotes: function(){
+      fetch('http://localhost:3000/notes', {
+        method: 'get',
+        headers: {
+          Authorization: `bearer ${this.$route.query.token.token}`
+        }
+      })
+          .then(response => response.json())
+          .then(data => {
+            this.notes = data
+          })
+    },
+    newNote: function(){
+      fetch('http://localhost:3000/notes', {
+        method: 'post',
+        headers: {
+          Authorization: `bearer ${this.$route.query.token.token}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({message: this.message})
+      })
+          .then(response => response.json())
+          .then(data => {
+            this.notes = data
+            this.getNotes()
+          })
+    },
+    deleteNote: function(event){
+      const id = event.target.id
 
-  }
+      fetch(`http://localhost:3000/notes/${id}`, {
+        method: "delete",
+        headers: {
+          authorization: `Bearer ${this.$route.query.token.token}`,
+          "Content-Type": "application/json"
+        },
+      })
+      .then(() =>
+      this.getNotes())
+    }
+    }
 }
 </script>
 
@@ -65,8 +98,8 @@ export default {
   font-size: 20px;
 }
 
-#saveAndDelete{
-  display: flex;
-  justify-content: center;
+.newNote{
+  width: 40%;
+  margin: 20px auto;
 }
 </style>
